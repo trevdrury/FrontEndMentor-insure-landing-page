55d12d7ce1324dd2cb522206f064983f389847e5
trevdrury <trevdrury@gmail.com>
2021-01-29 13:42:27 -0800

    finished styling for mobile view

--- src/app.js
+++ src/app.js
@@ -3,6 +3,13 @@ import "./scss/main.scss";
 let navToggle = document.querySelector(".nav-toggle");
 let navLinks = document.querySelector(".nav-links");
 let navBar = document.querySelector(".top-nav");
+let heroImg = document.querySelector("#hero__img");
+
+let width = window.innerWidth;
+
+if (width > 767) {
+  heroImg.setAttribute("src", "./assets/image-intro-desktop.jpg");
+}
 
 navToggle.addEventListener("click", function () {
   navToggle.classList.toggle("expanded");
